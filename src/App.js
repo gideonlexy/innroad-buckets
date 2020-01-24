1bd860a654bcab1c45359e5d88f5286162b2da7a
gideonlexy <gideon.lexy@tuzidi.org>
2020-01-24 17:32:55 +0300

    Feature: Modified the Application code

--- src/App.js
+++ src/App.js
@@ -5,22 +5,17 @@ import './App.css';
 
 class App extends Component {
   state = {
-    headings: ['Small Features', 'Booking Engine', 'Rates', 'CC Gateway', 'CRM/Email Marketing']
+    
   }
 
   render() {
-    const { headings } = this.state
+    
     return (
       <div>
         <nav className='nav'>
           <h2 className='wrapper'>InnRoad</h2>
         </nav>
         <div className='wrapper'>
-          <div className='heading'>
-            {headings.map((heading, index) => (
-              <h3 key={index}>{heading}</h3>
-            ))}
-          </div>
           <Card />
         </div>
       </div>
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,37 +1,56 @@
 import React, { Component } from 'react'
 import './card.styles.css'
 
+import axios from 'axios'
+
 class Card extends Component {
     state = {
-        title: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of',
-        message: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        
+        comments: [],
+        headings: ['Small Features', 'Booking Engine', 'Rates', 'CC Gateway', 'CRM/Email Marketing']
 
     }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/comments?_limit=15')
+        .then(res => {
+        this.setState({comments:res.data}, console.log(this.state))
+       
+        })
+        .catch(error => 
+            {console.log(error)})
+    }
+    
     render() {
-        const { title, message } = this.state
-        return (
-            <div className='App'>
-                <div className='main'>
-                    {[...Array(18)].map((element, index) => {
-                        return (
-                            <div key={index} className='card'>
-                                <h4>Some title</h4>
-                                {index % 2 === 0 ? title : message}
+        const { comments, headings } = this.state
+
+            return (
+                <div>
+                    <div className='heading'>
+                            {headings.map((heading, index) => (
+                            <h3 key={index}>{heading}</h3>
+                            ))}
+                        
+                    </div>
+                    <div className='App'>
+                        <div className='main'>
+              
+                            {comments.map(({id,name, body})  => 
+                            <div key={id} className='card'>
+                                <h3>{name}</h3>  
+                                {body}                 
                             </div>
-                        )
-                    })}
+                            )}
+                         </div>
+                    <div className='aside'>
+                            {comments.slice(0,2).map((element, index) => 
+                                <div key={index} className='card'>
+                                    <h3>{element.name}</h3>  
+                                        {element.body}             
+                                </div> )} 
+                        </div>
+                    </div>
                 </div>
-                <div className='aside'>
-                    {[...Array(2)].map((element, index) => {
-                        return (
-                            <div key={index} className='card'>
-                                <h4>Some title</h4>
-                                {index % 2 === 0 ? title : message}
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
         )
     }
 }
